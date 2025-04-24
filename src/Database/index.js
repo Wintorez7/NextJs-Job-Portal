4a5cb0b2839d71b588ae04ee6d7589b76c89d764
Wintorez7 <liordx76@gmail.com>
2025-04-24 21:44:19 +0530

    Hiding monogDB credential

--- src/Database/index.js
+++ src/Database/index.js
@@ -1,8 +1,8 @@
 const { default: mongoose } = require("mongoose");
 
-
+ 
 const connectToDb = async() => {
-    const connectionURL = "mongodb+srv://mohanKumhar:MK2025@cluster0.zlg15hi.mongodb.net/";
+    const connectionURL = process.env.MONGODB_URI;
     mongoose.connect(connectionURL)
          .then(() => console.log('Database Connection Successfull'))
          .catch((e) => console.log(e))
