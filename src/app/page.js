8c37d117a0354fc681a0d6e4124d97837f53cfb9
Wintorez7 <liordx76@gmail.com>
2025-11-19 20:07:37 +0530

    working on Job Portal project

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,18 @@
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation';
-
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function Home() {
-
-  const user = await currentUser()
-  console.log(user,'currentuser');
+  const user = await currentUser();
+  console.log(user, "currentuser");
 
   const profileInfo = null;
-  if(user && !profileInfo?._id) redirect('/onboard')
+  if (user && !profileInfo?._id) redirect("/onboard");
 
-return (
+  return (
     <div>
       <h1>Main Content</h1>
     </div>
   );
 }
 
-export default Home
+export default Home;
